46a30c15885f436190dd7ea84c51faa8ef94abce
'Gr?gory Tr?lat <gregory.trelat@lolitech.fr>
2009-07-10 11:08:33 +0200

    removed dead hidden files

---
+++ tests/build.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+STFILE=$1
+
+shift
+
+CFLAGS=$*
+
+
+CC=gcc
+#CC=i686-mingw32-gcc
+
+../iec2c $STFILE -I ../lib 
+#2>/dev/null
+
+$CC -I ../lib -c STD_RESSOURCE.c $CFLAGS
+
+$CC -I ../lib -c  STD_CONF.c $CFLAGS
+
+$CC -I ../lib -c  plc.c $CFLAGS
+
+$CC -I ../lib main.c STD_CONF.o STD_RESSOURCE.o plc.o $CFLAGS -l rt -o test
