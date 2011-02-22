0a171ab35a0c0cf1ffc49915f414b4407d72094f
edouard <edouard@localhost>
2011-02-22 15:19:41 +0100

    A forgotten case in type size getter switch prevented debugging outputs.

---
+++ lib/iec_types.h
@@ -0,0 +1,64 @@
+#ifndef IEC_TYPES_H
+#define IEC_TYPES_H
+
+#include <limits.h>
+#include <float.h>
+#include <time.h>
+#include <stdint.h>
+
+/*********************/
+/*  IEC Types defs   */
+/*********************/
+
+typedef uint8_t  IEC_BOOL;
+
+typedef int8_t    IEC_SINT;
+typedef int16_t   IEC_INT;
+typedef int32_t   IEC_DINT;
+typedef int64_t   IEC_LINT;
+
+typedef uint8_t    IEC_USINT;
+typedef uint16_t   IEC_UINT;
+typedef uint32_t   IEC_UDINT;
+typedef uint64_t   IEC_ULINT;
+
+typedef uint8_t    IEC_BYTE;
+typedef uint16_t   IEC_WORD;
+typedef uint32_t   IEC_DWORD;
+typedef uint64_t   IEC_LWORD;
+
+typedef float    IEC_REAL;
+typedef double   IEC_LREAL;
+
+
+#if !defined __timespec_defined && !defined __time_t_defined
+# define __timespec_defined     1
+
+struct timespec
+  {
+    long int tv_sec;            /* Seconds.  */
+    long int tv_nsec;           /* Nanoseconds.  */
+  };
+
+#endif
+
+typedef struct timespec IEC_TIME;
+typedef struct timespec IEC_DATE;
+typedef struct timespec IEC_DT;
+typedef struct timespec IEC_TOD;
+
+#ifndef STR_MAX_LEN
+#define STR_MAX_LEN 126
+#endif
+
+#ifndef STR_LEN_TYPE
+#define STR_LEN_TYPE int8_t
+#endif
+
+typedef STR_LEN_TYPE __strlen_t;
+typedef struct {
+    __strlen_t len;
+    uint8_t body[STR_MAX_LEN];
+} IEC_STRING;
+
+#endif /*IEC_TYPES_H*/
