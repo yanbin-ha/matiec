872149535414c46820541619227ade12f11836a8
Mario de Sousa <msousa@fe.up.pt>
2011-04-14 17:35:25 +0100

    Adding tracking of filename in which token is located.

---
+++ absyntax/absyntax.cc
@@ -0,0 +1,266 @@
+/*
+ *  matiec - a compiler for the programming languages defined in IEC 61131-3
+ *  Copyright (C) 2003-2011  Mario de Sousa (msousa@fe.up.pt)
+ *  Copyright (C) 2007-2011  Laurent Bessard and Edouard Tisserant
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *
+ * This code is made available on the understanding that it will not be
+ * used in safety-critical situations without a full and competent review.
+ */
+
+/*
+ * An IEC 61131-3 compiler.
+ *
+ * Based on the
+ * FINAL DRAFT - IEC 61131-3, 2nd Ed. (2001-12-10)
+ *
+ */
+
+/*
+ * Definition of the Abstract Syntax data structure components
+ */
+
+#include <stdio.h>
+#include <stdlib.h>	/* required for exit() */
+#include <string.h>
+
+#include "absyntax.hh"
+//#include "../stage1_2/iec.hh" /* required for BOGUS_TOKEN_ID, etc... */
+#include "visitor.hh"
+
+#define ABORT(str) {printf("ERROR: %s\n", str); exit(0);}
+
+
+
+
+
+
+/* The base class of all symbols */
+symbol_c::symbol_c(
+                   int first_line, int first_column, const char *ffile,
+                   int last_line,  int last_column,  const char *lfile) {
+  this->first_file   = ffile,
+  this->first_line   = first_line;
+  this->first_column = first_column;
+  this->last_file    = lfile,
+  this->last_line    = last_line;
+  this->last_column  = last_column;
+}
+
+
+
+token_c::token_c(const char *value, 
+                 int fl, int fc, const char *ffile,
+                 int ll, int lc, const char *lfile)
+  :symbol_c(fl, fc, ffile, ll, lc, lfile) {
+  this->value = value;
+//  printf("New token: %s\n", value);
+}
+
+
+
+
+
+
+list_c::list_c(
+               int fl, int fc, const char *ffile,
+               int ll, int lc, const char *lfile)
+  :symbol_c(fl, fc, ffile, ll, lc, lfile) {
+  n = 0;
+  elements = NULL;
+}
+
+list_c::list_c(symbol_c *elem, 
+               int fl, int fc, const char *ffile,
+               int ll, int lc, const char *lfile)
+  :symbol_c(fl, fc, ffile, ll, lc, lfile) {
+  n = 0;
+  elements = NULL;
+  add_element(elem);
+}
+
+/* insert a new element */
+void list_c::add_element(symbol_c *elem) {
+//printf("list_c::add_element()\n");
+  n++;
+  elements = (symbol_c **)realloc(elements, n * sizeof(symbol_c *));
+  if (elements == NULL)
+    ABORT("Out of memory");
+  elements[n - 1] = elem;
+ 
+  if (elem == NULL)
+    return;
+
+  /* adjust the location parameters, taking into account the new element. */
+  if ((first_line == elem->first_line) &&
+      (first_column > elem->first_column)) {
+    first_column = elem->first_column;
+  }
+  if (first_line > elem->first_line) {
+    first_line = elem->first_line;
+    first_column = elem->first_column;
+  }
+  if ((last_line == elem->last_line) &&
+      (last_column < elem->last_column)) {
+    last_column = elem->last_column;
+  }
+  if (last_line < elem->last_line) {
+    last_line = elem->last_line;
+    last_column = elem->last_column;
+  }
+}
+
+
+
+
+#define SYM_LIST(class_name_c)									\
+class_name_c::class_name_c(									\
+                           int fl, int fc, const char *ffile,					\
+                           int ll, int lc, const char *lfile)					\
+                        :list_c(fl, fc, ffile, ll, lc, lfile) {}				\
+class_name_c::class_name_c(symbol_c *elem, 							\
+                           int fl, int fc, const char *ffile,					\
+                           int ll, int lc, const char *lfile)					\
+			:list_c(elem, fl, fc, ffile, ll, lc, lfile) {}				\
+void *class_name_c::accept(visitor_c &visitor) {return visitor.visit(this);}
+
+#define SYM_TOKEN(class_name_c)									\
+class_name_c::class_name_c(const char *value, 							\
+                           int fl, int fc, const char *ffile,					\
+                           int ll, int lc, const char *lfile)					\
+			:token_c(value, fl, fc, ffile, ll, lc, lfile) {}			\
+void *class_name_c::accept(visitor_c &visitor) {return visitor.visit(this);}
+
+#define SYM_REF0(class_name_c)									\
+class_name_c::class_name_c(									\
+                           int fl, int fc, const char *ffile,					\
+                           int ll, int lc, const char *lfile)					\
+			  :symbol_c(fl, fc, ffile, ll, lc, lfile) {}				\
+void *class_name_c::accept(visitor_c &visitor) {return visitor.visit(this);}
+
+
+#define SYM_REF1(class_name_c, ref1)								\
+class_name_c::class_name_c(symbol_c *ref1,							\
+                           int fl, int fc, const char *ffile,					\
+                           int ll, int lc, const char *lfile)					\
+			  :symbol_c(fl, fc, ffile, ll, lc, lfile) {				\
+  this->ref1 = ref1;										\
+}												\
+void *class_name_c::accept(visitor_c &visitor) {return visitor.visit(this);}
+
+
+#define SYM_REF2(class_name_c, ref1, ref2)							\
+class_name_c::class_name_c(symbol_c *ref1,							\
+			   symbol_c *ref2,							\
+                           int fl, int fc, const char *ffile,					\
+                           int ll, int lc, const char *lfile)					\
+			  :symbol_c(fl, fc, ffile, ll, lc, lfile) {				\
+  this->ref1 = ref1;										\
+  this->ref2 = ref2;										\
+}												\
+void *class_name_c::accept(visitor_c &visitor) {return visitor.visit(this);}
+
+
+#define SYM_REF3(class_name_c, ref1, ref2, ref3)						\
+class_name_c::class_name_c(symbol_c *ref1,							\
+			   symbol_c *ref2,							\
+			   symbol_c *ref3,							\
+                           int fl, int fc, const char *ffile,					\
+                           int ll, int lc, const char *lfile)					\
+			  :symbol_c(fl, fc, ffile, ll, lc, lfile) {				\
+  this->ref1 = ref1;										\
+  this->ref2 = ref2;										\
+  this->ref3 = ref3;										\
+}												\
+void *class_name_c::accept(visitor_c &visitor) {return visitor.visit(this);}
+
+
+#define SYM_REF4(class_name_c, ref1, ref2, ref3, ref4)						\
+class_name_c::class_name_c(symbol_c *ref1,							\
+			   symbol_c *ref2,							\
+			   symbol_c *ref3,							\
+			   symbol_c *ref4,							\
+                           int fl, int fc, const char *ffile,					\
+                           int ll, int lc, const char *lfile)					\
+			  :symbol_c(fl, fc, ffile, ll, lc, lfile) {				\
+  this->ref1 = ref1;										\
+  this->ref2 = ref2;										\
+  this->ref3 = ref3;										\
+  this->ref4 = ref4;										\
+}												\
+void *class_name_c::accept(visitor_c &visitor) {return visitor.visit(this);}
+
+
+#define SYM_REF5(class_name_c, ref1, ref2, ref3, ref4, ref5)					\
+class_name_c::class_name_c(symbol_c *ref1,							\
+			   symbol_c *ref2,							\
+			   symbol_c *ref3,							\
+			   symbol_c *ref4,							\
+			   symbol_c *ref5,							\
+                           int fl, int fc, const char *ffile,					\
+                           int ll, int lc, const char *lfile)					\
+			  :symbol_c(fl, fc, ffile, ll, lc, lfile) {				\
+  this->ref1 = ref1;										\
+  this->ref2 = ref2;										\
+  this->ref3 = ref3;										\
+  this->ref4 = ref4;										\
+  this->ref5 = ref5;										\
+}												\
+void *class_name_c::accept(visitor_c &visitor) {return visitor.visit(this);}
+
+
+
+#define SYM_REF6(class_name_c, ref1, ref2, ref3, ref4, ref5, ref6)				\
+class_name_c::class_name_c(symbol_c *ref1,							\
+			   symbol_c *ref2,							\
+			   symbol_c *ref3,							\
+			   symbol_c *ref4,							\
+			   symbol_c *ref5,							\
+			   symbol_c *ref6,							\
+                           int fl, int fc, const char *ffile,					\
+                           int ll, int lc, const char *lfile)					\
+			  :symbol_c(fl, fc, ffile, ll, lc, lfile) {				\
+  this->ref1 = ref1;										\
+  this->ref2 = ref2;										\
+  this->ref3 = ref3;										\
+  this->ref4 = ref4;										\
+  this->ref5 = ref5;										\
+  this->ref6 = ref6;										\
+}												\
+void *class_name_c::accept(visitor_c &visitor) {return visitor.visit(this);}
+
+
+
+#include "absyntax.def"
+
+
+
+
+#undef SYM_LIST
+#undef SYM_TOKEN
+#undef SYM_TOKEN
+#undef SYM_REF0
+#undef SYM_REF1
+#undef SYM_REF2
+#undef SYM_REF3
+#undef SYM_REF4
+#undef SYM_REF5
+#undef SYM_REF6
+
+
+
+
+
---
+++ absyntax/absyntax.hh
@@ -0,0 +1,278 @@
+/*
+ *  matiec - a compiler for the programming languages defined in IEC 61131-3
+ *  Copyright (C) 2003-2011  Mario de Sousa (msousa@fe.up.pt)
+ *  Copyright (C) 2007-2011  Laurent Bessard and Edouard Tisserant
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *
+ * This code is made available on the understanding that it will not be
+ * used in safety-critical situations without a full and competent review.
+ */
+
+/*
+ * An IEC 61131-3 compiler.
+ *
+ * Based on the
+ * FINAL DRAFT - IEC 61131-3, 2nd Ed. (2001-12-10)
+ *
+ */
+
+/*
+ * Declaration of the Abstract Syntax data structure components
+ */
+
+/*
+ * ABSYNTAX.H
+ *
+ * This generates the parse tree structure used to bind the components
+ * identified by Bison in the correct syntax order. At the end of the
+ * Bison analysis the tree is walked in a sequential fashion generating
+ * the relavent code.
+ */
+
+#ifndef _ABSYNTAX_HH
+#define _ABSYNTAX_HH
+
+
+#include <stdio.h> // required for NULL
+
+/* Forward declaration of the visitor interface
+ * dclared in the visitor.hh file
+ * We cannot include the visitor.hh file, as it will
+ * include this same file first, as it too requires references
+ * to the abstract syntax classes defined here.
+ */
+class visitor_c; // forward declaration
+
+
+class symbol_c; // forward declaration
+
+
+
+/* The base class of all symbols */
+class symbol_c {
+
+  public:
+    /*
+     * Line number for the purposes of error checking
+     */
+    int first_line;
+    int first_column;
+    const char *first_file;  /* filename referenced by first line/column */
+    int last_line;
+    int last_column;
+    const char *last_file;  /* filename referenced by last line/column */
+
+  public:
+    /* default constructor */
+    symbol_c(int fl = 0, int fc = 0, const char *ffile = NULL /* filename */,
+             int ll = 0, int lc = 0, const char *lfile = NULL /* filename */
+            );
+
+    /* default destructor */
+    /* must be virtual so compiler does not complain... */ 
+    virtual ~symbol_c(void) {return;};
+
+    virtual void *accept(visitor_c &visitor) {return NULL;};
+};
+
+
+class token_c: public symbol_c {
+  public:
+    /* the value of the symbol. */
+    const char *value;
+
+  public:
+    token_c(const char *value, 
+            int fl = 0, int fc = 0, const char *ffile = NULL /* filename */,
+            int ll = 0, int lc = 0, const char *lfile = NULL /* filename */
+           );
+};
+
+
+ /* a list of symbols... */
+class list_c: public symbol_c {
+  public:
+    int n;
+    symbol_c **elements;
+
+  public:
+    list_c(int fl = 0, int fc = 0, const char *ffile = NULL /* filename */,
+           int ll = 0, int lc = 0, const char *lfile = NULL /* filename */
+          );
+
+    list_c(symbol_c *elem, 
+	   int fl = 0, int fc = 0, const char *ffile = NULL /* filename */,
+           int ll = 0, int lc = 0, const char *lfile = NULL /* filename */
+          );
+     /* insert a new element */
+    virtual void add_element(symbol_c *elem);
+};
+
+
+
+
+#define SYM_LIST(class_name_c)												\
+class class_name_c:	public list_c {											\
+  public:														\
+    class_name_c(													\
+                 int fl = 0, int fc = 0, const char *ffile = NULL /* filename */, 					\
+                 int ll = 0, int lc = 0, const char *lfile = NULL /* filename */);					\
+    class_name_c(symbol_c *elem, 											\
+                 int fl = 0, int fc = 0, const char *ffile = NULL /* filename */, 					\
+                 int ll = 0, int lc = 0, const char *lfile = NULL /* filename */);					\
+    virtual void *accept(visitor_c &visitor);										\
+};
+
+
+#define SYM_TOKEN(class_name_c)												\
+class class_name_c: 	public token_c {										\
+  public:														\
+    class_name_c(const char *value, 											\
+                 int fl = 0, int fc = 0, const char *ffile = NULL /* filename */, 					\
+                 int ll = 0, int lc = 0, const char *lfile = NULL /* filename */);					\
+    virtual void *accept(visitor_c &visitor);										\
+};
+
+
+#define SYM_REF0(class_name_c)												\
+class class_name_c: public symbol_c {											\
+  public:														\
+    class_name_c(		 											\
+                 int fl = 0, int fc = 0, const char *ffile = NULL /* filename */, 					\
+                 int ll = 0, int lc = 0, const char *lfile = NULL /* filename */);					\
+    virtual void *accept(visitor_c &visitor);										\
+};
+
+
+#define SYM_REF1(class_name_c, ref1)											\
+class class_name_c: public symbol_c {											\
+  public:														\
+    symbol_c *ref1;													\
+  public:														\
+    class_name_c(symbol_c *ref1,											\
+                 int fl = 0, int fc = 0, const char *ffile = NULL /* filename */, 					\
+                 int ll = 0, int lc = 0, const char *lfile = NULL /* filename */);					\
+    virtual void *accept(visitor_c &visitor);										\
+};
+
+
+#define SYM_REF2(class_name_c, ref1, ref2)										\
+class class_name_c: public symbol_c {											\
+  public:														\
+    symbol_c *ref1;													\
+    symbol_c *ref2;													\
+  public:														\
+    class_name_c(symbol_c *ref1,											\
+		 symbol_c *ref2 = NULL,											\
+                 int fl = 0, int fc = 0, const char *ffile = NULL /* filename */, 					\
+                 int ll = 0, int lc = 0, const char *lfile = NULL /* filename */);					\
+    virtual void *accept(visitor_c &visitor);										\
+};
+
+
+#define SYM_REF3(class_name_c, ref1, ref2, ref3)									\
+class class_name_c: public symbol_c {											\
+  public:														\
+    symbol_c *ref1;													\
+    symbol_c *ref2;													\
+    symbol_c *ref3;													\
+  public:														\
+    class_name_c(symbol_c *ref1,											\
+		 symbol_c *ref2,											\
+		 symbol_c *ref3,											\
+                 int fl = 0, int fc = 0, const char *ffile = NULL /* filename */, 					\
+                 int ll = 0, int lc = 0, const char *lfile = NULL /* filename */);					\
+    virtual void *accept(visitor_c &visitor);										\
+};
+
+
+#define SYM_REF4(class_name_c, ref1, ref2, ref3, ref4)									\
+class class_name_c: public symbol_c {											\
+  public:														\
+    symbol_c *ref1;													\
+    symbol_c *ref2;													\
+    symbol_c *ref3;													\
+    symbol_c *ref4;													\
+  public:														\
+    class_name_c(symbol_c *ref1,											\
+		 symbol_c *ref2,											\
+		 symbol_c *ref3,											\
+		 symbol_c *ref4 = NULL,											\
+                 int fl = 0, int fc = 0, const char *ffile = NULL /* filename */, 					\
+                 int ll = 0, int lc = 0, const char *lfile = NULL /* filename */);					\
+    virtual void *accept(visitor_c &visitor);										\
+};
+
+
+#define SYM_REF5(class_name_c, ref1, ref2, ref3, ref4, ref5)								\
+class class_name_c: public symbol_c {											\
+  public:														\
+    symbol_c *ref1;													\
+    symbol_c *ref2;													\
+    symbol_c *ref3;													\
+    symbol_c *ref4;													\
+    symbol_c *ref5;													\
+  public:														\
+    class_name_c(symbol_c *ref1,											\
+		 symbol_c *ref2,											\
+		 symbol_c *ref3,											\
+		 symbol_c *ref4,											\
+		 symbol_c *ref5,											\
+                 int fl = 0, int fc = 0, const char *ffile = NULL /* filename */, 					\
+                 int ll = 0, int lc = 0, const char *lfile = NULL /* filename */);					\
+    virtual void *accept(visitor_c &visitor);										\
+};
+
+
+#define SYM_REF6(class_name_c, ref1, ref2, ref3, ref4, ref5, ref6)							\
+class class_name_c: public symbol_c {											\
+  public:														\
+    symbol_c *ref1;													\
+    symbol_c *ref2;													\
+    symbol_c *ref3;													\
+    symbol_c *ref4;													\
+    symbol_c *ref5;													\
+    symbol_c *ref6;													\
+  public:														\
+    class_name_c(symbol_c *ref1,											\
+		 symbol_c *ref2,											\
+		 symbol_c *ref3,											\
+		 symbol_c *ref4,											\
+		 symbol_c *ref5,											\
+		 symbol_c *ref6 = NULL,											\
+                 int fl = 0, int fc = 0, const char *ffile = NULL /* filename */, 					\
+                 int ll = 0, int lc = 0, const char *lfile = NULL /* filename */);					\
+    virtual void *accept(visitor_c &visitor);										\
+};
+
+
+#include "absyntax.def"
+
+
+
+#undef SYM_LIST
+#undef SYM_TOKEN
+#undef SYM_REF0
+#undef SYM_REF1
+#undef SYM_REF2
+#undef SYM_REF3
+#undef SYM_REF4
+#undef SYM_REF5
+#undef SYM_REF6
+
+
+
+#endif /*  _ABSYNTAX_HH */
